9cf4e4967a63d71f2cf17d5553e15b1bf0102a11
Brian Dang <bdang@amazon.com>
2016-01-23 11:07:10 -0800

    Base logic for query API. Untested.

--- src/main/java/com/bdang/storage/GremlinAccessor.java
+++ src/main/java/com/bdang/storage/GremlinAccessor.java
@@ -7,9 +7,11 @@ import org.apache.tinkerpop.gremlin.driver.Result;
 import org.apache.tinkerpop.gremlin.driver.ResultSet;
 import org.apache.tinkerpop.gremlin.structure.Edge;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class GremlinAccessor implements Accessor {
 
@@ -26,22 +28,20 @@ public class GremlinAccessor implements Accessor {
             // 1. Ensure both subject and object are existing vertices. If not, create them.
             // 2. Create an edge if it isn't already there
             Map<String,Object> params = new HashMap<>();
-            params.put("?", fact.getSubject());
-            Result result = client.submit("g.V().has('name','?')", params).one();
+            params.put("x", fact.getSubject());
+            params.put("y", fact.getRel().toString());
+            params.put("z", fact.getObject());
+            Result result = client.submit("g.V().has('name','x')", params).one();
             if (result == null) {
-                client.submit("g.addV('name','?')", params);
+                client.submit("g.addV('name','x')", params);
             }
 
-            params.put("?", fact.getObject());
-            result = client.submit("g.V().has('name','?')", params).one();
+            result = client.submit("g.V().has('name','z')", params).one();
             if (result == null) {
-                client.submit("g.addV('name','?')", params);
+                client.submit("g.addV('name','z')", params);
             }
 
-            result = client.submit("g.E().hasLabel('" + fact.getRel().toString()
-                    + "').and(outV().has('name','" + fact.getSubject()
-                    + "'),inV().has('name','" + fact.getObject()
-                    + "'))", params).one();
+            result = client.submit("g.E().hasLabel('y').and(outV().has('name','x'),inV().has('name','z'))", params).one();
             if (result == null) {
                 result = client.submit("g.withSideEffect('a',g.V().has('name','?')).V().has('name','?').addOutE('?','a')").one();
             }
@@ -109,7 +109,29 @@ public class GremlinAccessor implements Accessor {
     }
 
     public List<String> find(Fact query) {
-        return null;
+        final List<String> findResults = new ArrayList<>();
+
+        Cluster cluster = Cluster.open();
+        Client client = cluster.connect();
+
+        try {
+            Map<String,Object> params = new HashMap<>();
+            params.put("x", query.getSubject());
+            params.put("y", query.getRel().toString());
+            params.put("z", query.getObject());
+            ResultSet results = client.submit("g.V().has('name','x').inV('isa').as('a').outV('y').has('name','z').inV('y').where(within('a'))", params);
+            results.forEach(new Consumer<Result>() {
+                @Override
+                public void accept(Result result) {
+                    findResults.add(result.getVertex().value("name").toString());
+                }
+            });
+        } finally {
+            client.close();
+            cluster.close();
+        }
+
+        return findResults;
     }
 
     public long count(Fact query) {
@@ -120,8 +142,10 @@ public class GremlinAccessor implements Accessor {
 
         try {
             Map<String,Object> params = new HashMap<>();
-            params.put("?", query.getSubject());
-            ResultSet results = client.submit("g.V().has('name','?').in('isa').out('?').where(.....).count()", params);
+            params.put("x", query.getSubject());
+            params.put("y", query.getRel().toString());
+            params.put("z", query.getObject());
+            ResultSet results = client.submit("g.V().has('name','x').inV('isa').outV('y').has('name','z').count()", params);
             Result result = results.one();
             if (result != null) {
                 count = result.getLong();
